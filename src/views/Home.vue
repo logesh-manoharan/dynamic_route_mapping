b94786f1ea910a9b276ef0848ba2ec8fee3aa19c
Manoharan <logeshmano189@gmail.com>
2021-03-05 19:06:45 +0530

    Dynamic_Route_Basic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Role</th>
+      </tr>
+
+      <tr v-for="(user, index) in UsersList" :key="index">
+        <td>{{user.name}}</td>
+        <td>{{user.age}}</td>
+        <td>{{user.role}}</td>
+        <td>
+          <router-link :to="`/user/${index}`">User-{{index}}</router-link>
+        </td>
+      </tr>
+
+      <th>
+
+      </th>
+    </table>
+    <!-- <b-table striped hover>
+      <b-tr>
+        <b-th>Name</b-th>
+        <b-th>Age</b-th>
+        <b-th>Role</b-th>
+      </b-tr>
+      <b-tr v-for="(user, index) in UsersList" :key="index">
+        <b-td>{{user.name}}</b-td>
+        <b-td>{{user.age}}</b-td>
+        <b-td>{{user.role}}</b-td>
+        <b-td>
+          <router-link :to="`/user/${index}`">User-{{index}}</router-link>
+        </b-td>
+      </b-tr>
+    </b-table> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      UsersList: [{name: 'logesh', age: 20, role: 'student'},
+      {name: 'Manikandan', age: 35, role: 'faculty'},
+      {name: 'Kaala', age: 21, role: 'student'},
+      {name: 'Mano', age: 21, role: 'student'},
+      {name: 'Karthi', age: 40, role: 'student '}]
+    }
   }
 }
 </script>
+
+<style scoped>
+table {
+  width: 75%;
+  margin: 0px 150px;
+}
+
+tr:nth-child(even) 
+{
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background: #f2f2f2;
+}
+
+td, th {
+  text-align: left;
+  padding: 5px;
+}
+
+</style>
---
+++ src/views/UserComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>User Component</h1>  
+        <p>User - {{$route.params.id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserComponent"
+}
+</script>
